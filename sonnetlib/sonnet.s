b5fd555fb9c091e964666b76ef38513e6c78cd2c
hedeon <hedeon>
2015-02-11 21:38:16 +0000

    Added TODO list

--- sonnetlib/sonnet.s
+++ sonnetlib/sonnet.s
@@ -7,8 +7,8 @@ OMBAR		EQU $300
 OTWR		EQU $308
 WP_CONTROL	EQU $F48		
 WP_TRIG01	EQU $c0000000
-mh_Upper	EQU 24
 MEMF_PPC	EQU $1000
+StackSize	EQU $80000
 	
 	incdir	include:
 	include	lvo/exec_lib.i
@@ -18,6 +18,9 @@ MEMF_PPC	EQU $1000
 	include exec/resident.i
 	include	exec/memory.i
 	include pci.i
+	include	lvo/expansion_lib.i
+	include	libraries/configvars.i
+	include	exec/execbase.i
 
 	
 	XREF	ConfirmInterrupt
@@ -49,12 +52,12 @@ ROMTAG:
 INIT	movem.l d1-a6,-(a7)
 	move.l 4.w,a6
 	
-	lea 322(a6),a0
+	lea MemList(a6),a0
 	lea MemName(pc),a1
 	jsr _LVOFindName(a6)
 	tst.l d0
 	bne.s Exit
-	lea 322(a6),a0
+	lea MemList(a6),a0
 	lea PCIMem(pc),a1
 	jsr _LVOFindName(a6)
 	tst.l d0
@@ -76,6 +79,26 @@ FndMem	move.l d0,d7
 	beq.s Exit
 	move.l d0,a5
 	
+	lea ExpLib(pc),a1
+	moveq.l #27,d0
+	jsr _LVOOpenLibrary(a6)			;Open expansion.library
+	tst.l d0
+	beq Exit2
+
+	move.l d0,a6
+	sub.l a0,a0
+	move.l #$89e,d0				;ELBOX
+	moveq.l #33,d1				;Mediator MKII
+	jsr _LVOFindConfigDev(a6)		;Find A3000/A4000 mediator (for now)
+	move.l 4.w,a6
+	tst.l d0
+	beq Exit2
+
+	move.l d0,a1
+	move.l cd_BoardAddr(a1),d0		;Start address Configspace Mediator
+	lea MediatorBase(pc),a2
+	move.l d0,(a2)
+	
 	move.l PCI_List(a5),a2
 Loop1	move.l LN_SUCC(a2),d6
 	beq.s Exit2
@@ -86,14 +109,14 @@ Loop2	move.l d6,a2
 	bra.s Loop1	
 	
 Sonnet	move.l d7,a0
-	move.l mh_Upper(a0),d1
+	move.l MH_UPPER(a0),d1
 	sub.l #$10000,d1
 	and.w #0,d1
 	move.l d1,a1
 	move.l #$10000,d0
 	jsr _LVOAllocAbs(a6)
 	tst.l d0
-	beq.s Exit2
+	beq Exit2
 
 	move.l d0,a4
 	move.l a4,a1
@@ -134,7 +157,7 @@ Wait	move.l $6004(a1),d5
 	cmp.l #"Boon",d5
 	bne.s Wait
 	
-NoReloc	move.l #$80000,d7			;Set stack
+NoReloc	move.l #StackSize,d7			;Set stack
 	move.l $6008(a1),d5
 	add.l d7,d5
 	move.l $600c(a1),d6
@@ -155,30 +178,31 @@ Reloc	moveq.l #16,d0
 		
 	move.l a0,a1
 	move.l d5,a0
-	move.w #$0a32,8(a0)			;DEBUG $0a32
-	move.l a1,10(a0)
+	move.w #$0a32,LN_TYPE(a0)			;DEBUG $0a32
+	move.l a1,LN_NAME(a0)
 	move.w #MEMF_PUBLIC|MEMF_FAST|MEMF_PPC,14(a0)
-	lea 32(a0),a1
-	move.l a1,16(a0)
+	lea MH_SIZE(a0),a1
+	move.l a1,MH_FIRST(a0)
 	clr.l (a1)
 	move.l d6,d1
 	sub.l #32,d1
 	move.l d1,4(a1)
-	move.l a1,20(a0)
+	move.l a1,MH_LOWER(a0)
 	add.l a0,d6
-	move.l d6,24(a0)
-	move.l d1,28(a0)
+	move.l d6,MH_UPPER(a0)
+	move.l d1,MH_FREE(a0)
 	move.l a0,a1	
 	move.l a0,a4
 	
 	jsr _LVODisable(a6)	
-	lea 322(a6),a0
+	lea MemList(a6),a0
 	tst.l d4
 	beq.s NoPCILb
 	
 	move.l d4,a2
+	sub.l #StackSize,d5
 	move.l d5,PCI_SPACE0(a2)
-	moveq.l #1,d6
+	moveq.l #0,d6
 	sub.l d7,d6		
 	move.l d6,PCI_SPACELEN0(a2)
 NoPCILb	jsr _LVOEnqueue(a6)
@@ -223,6 +247,7 @@ NoLib	move.l a4,a1
 	jsr _LVORemove(a6)
 	move.w #$0a01,8(a4)
 	move.l a4,a1
+	lea MemList(a6),a0
 	jsr _LVOEnqueue(a6)
 	jsr _LVOEnable(a6)		
 
@@ -232,11 +257,12 @@ NoLib	move.l a4,a1
 
 ;********************************************************************************************
 
-Dirty	moveq.l #0,d2				;Make less dirty by using expansion.library
-	moveq.l #$3f,d1				;Now follows some nasty absolute values
-	move.l #$40000000,a0			;Start Mediator config
-	move.b #$60,(a0)			;Start PCI Mem ($60000000)
-CpLoop	move.l #$40800000,a4			;Start PCI config
+Dirty	move.l MediatorBase(pc),a0
+	moveq.l #0,d2
+	moveq.l #$3f,d1
+	move.b #$60,(a0)			;Start address PCI Mem ($60000000)
+CpLoop	move.l a0,a4
+	add.l #$800000,a4			;Start address PCI config
 	move.l d2,d0
 	lsl.l #3,d0
 	lsl.l #8,d0
@@ -285,6 +311,9 @@ VooDoo3	movem.l d0-a6,-(a7)
 	movem.l (a7)+,d0-a6
 	bra VooDone
 
+
+ExpLib	dc.b "expansion.library",0
+	cnop	0,2
 pcilib	dc.b "pci.library",0
 	cnop	0,2
 MemName	dc.b "Sonnet memory",0
@@ -299,12 +328,12 @@ Open	move.l	a6,d0
 	tst.l	d0
 	beq.s	NoA6
 	move.l	d0,a6
-	addq.w	#1,32(a6)
+	addq.w	#1,LIB_OPENCNT(a6)
 	bclr	#3,Buffer
 NoA6	rts
 
 Close	moveq.l #0,d0
-	subq.w	#1,32(a6)
+	subq.w	#1,LIB_OPENCNT(a6)
 	bne.s	NoExp
 	btst	#3,Buffer
 	bne.s	Expunge
@@ -363,8 +392,9 @@ Error	nop
 
 FindSonnet:
 	moveq.l #0,d2
-	moveq.l #$3f,d1				;Now follow some nasty absolute values
-CpxLoop	move.l #$40800000,a4
+	moveq.l #$3f,d1
+CpxLoop	move.l MediatorBase(pc),a4
+	add.l #$800000,a4
 	move.l d2,d0
 	lsl.l #3,d0
 	lsl.l #8,d0
@@ -479,7 +509,7 @@ ObtainSemaphoreSharedPPC	rts
 AttemptSemaphoreSharedPPC	rts
 ProcurePPC			rts
 VacatePPC			rts
-CauseInterrupt2			rts
+CauseInterrupt			rts
 CreatePoolPPC			rts
 DeletePoolPPC			rts
 AllocPooledPP			rts
@@ -496,8 +526,9 @@ DriverID
 	dc.b "WarpUp hardware driver for Sonnet Crescendo 7200 PCI",0
 	cnop	0,2
 
-Buffer	DS.L	1
-PowerPCBase	DS.L	1
+Buffer		ds.l	1
+PowerPCBase	ds.l	1
+MediatorBase	ds.l	1
 
 DATATABLE:
 	INITBYTE	LN_TYPE,NT_LIBRARY
@@ -665,7 +696,7 @@ EndFlag	dc.l	$ffffffff
 LibName
 	dc.b	"sonnet.library",0,0
 IDString
-	DC.B	"$VER: sonnet.library 1.0 (10-Feb-15)",0
+	DC.B	"$VER: sonnet.library 1.0 (11-Feb-15)",0
 	cnop	0,4
 EndCp	nop
 	end
