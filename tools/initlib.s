2b9e7c1a72a9045af64b457a0ab367c72074aa93
Dennis Boon <dennisvdboon@gmail.com>
2015-02-25 22:24:04 +0000

    This file just needs to start the library. Otherwise Mem attribute $1005 won't be availabe for our very first executed PPC program.

--- tools/initlib.s
+++ tools/initlib.s
@@ -1,6 +1,6 @@
-
-	include exec/exec_lib.i
-	include	sonnet_lib.i
+	incdir include:
+	include lvo/exec_lib.i
+	include	lvo/sonnet_lib.i
 
 
 	movem.l d0-a6,-(a7)
@@ -9,13 +9,7 @@
 	moveq.l #0,d0
 	jsr _LVOOpenLibrary(a6)
 	tst.l d0
-	move.l d0,a6
-	lea PPStrct(pc),a0
-	jsr _LVORunPPC(a6)
-
 	movem.l (a7)+,d0-a6
 	rts
 	
-Sonnet	dc.b "sonnet.library",0	
-PPStrct	dc.l 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-	dc.l 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+Sonnet	dc.b "sonnet.library",0
