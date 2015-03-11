676979fa18a58c06422bf0ae21202e40a90c0b96
Dennis Boon <dennisvdboon@gmail.com>
2015-03-11 23:16:10 +0000

    Improved templates.

--- tools/template_68k.s
+++ tools/template_68k.s
@@ -10,7 +10,7 @@ FUNC_CNT	SET	FUNC_CNT-6	* Standard offset-6 bytes each
 		include exec/exec_lib.i
 		include dos/dos_lib.i
 		
-		XDEF _SysBase,_DOSBase,_PowerPCBase
+		XREF _SysBase,_DOSBase,_PowerPCBase,_LinkerDB
 
 ;************************************************************************************************
 
@@ -19,30 +19,30 @@ FUNC_CNT	SET	FUNC_CNT-6	* Standard offset-6 bytes each
 ;************************************************************************************************		
 		
 		movem.l d0-a6,-(a7)			;Need to add WBMsg handling
-		lea Data,a4
+		lea _LinkerDB,a4
 		move.l 4.w,a6
-		move.l a6,_SysBase-Data(a4)
+		move.l a6,_SysBase
 		moveq.l #37,d0
 		lea DosLib(pc),a1
 		jsr _LVOOpenLibrary(a6)
 		tst.l d0
 		beq NoLib
-		move.l d0,_DOSBase-Data(a4)
+		move.l d0,_DOSBase
 		moveq.l #0,d0
 		lea PowerPCLib(pc),a1
 		jsr _LVOOpenLibrary(a6)
 		tst.l d0
 		beq NoLib
-		move.l d0,_PowerPCBase-Data(a4)
+		move.l d0,_PowerPCBase
 		
 		RUNPOWERPC PPC_Code
 		
 		move.l 4.w,a6
-NoLib		move.l _PowerPCBase-Data(a4),d0
+NoLib		move.l _PowerPCBase,d0
 		beq.s NoPower
 		move.l d0,a1
 		jsr _LVOCloseLibrary(a6)
-NoPower		move.l _DOSBase-Data(a4),d0
+NoPower		move.l _DOSBase,d0
 		beq.s NoDOS
 		move.l d0,a1
 		jsr _LVOCloseLibrary(a6)
@@ -50,19 +50,9 @@ NoPower		move.l _DOSBase-Data(a4),d0
 NoDOS		movem.l (a7)+,d0-a6
 		rts
 		
-		
+;************************************************************************************************		
+
 DosLib		dc.b "dos.library",0
 PowerPCLib	dc.b "sonnet.library",0		
 		
-;************************************************************************************************
-
-		section ppcdata,data,$1005	;MUST be in Sonnet memory!
-		
-;************************************************************************************************
-
-Data		ds.l	1
-_SysBase	ds.l 	1
-_DOSBase	ds.l	1
-_PowerPCBase	ds.l	1
-
-		
+;************************************************************************************************		
--- tools/template_ppc.p
+++ tools/template_ppc.p
@@ -1,16 +1,52 @@
 
-.global		@_PPC_Code
+.global		@_PPC_Code,_SysBase,_DOSBase,_PowerPCBase,_LinkerDB
 
-#;************************************************************************************************
+
+#************************************************************************************************
 
 #.section "ppccode","acrx",0x1005	#MUST be in Sonnet memory!
 .section "ppccode","acrx"		#code ($3E9) plus memory attributes not working atm.
 		
-#;************************************************************************************************
+#************************************************************************************************
 
 @_PPC_Code:
 
 .long			PPC_Code
 
-PPC_Code:		nop
+PPC_Code:		stw	r2,20(r1)
+			mflr	r0
+			stw	r0,8(r1)
+			mfcr	r0
+			stw	r0,4(r1)
+			stw	r13,-4(r1)
+			subi	r13,r1,4
+			stwu	r1,-1024(r1)
+
+			nop
+			
+			lwz	r1,0(r1)
+			lwz	r13,-4(r1)
+			lwz	r0,8(r1)
+			mtlr	r0
+			lwz	r0,4(r1)
+			mtcr	r0
+			lwz	r2,20(r1)
+			
 			blr
+			
+
+#************************************************************************************************
+
+.section "ppcdata","adrw"		#MUST be in Sonnet memory! (See note above)
+		
+#************************************************************************************************
+
+_LinkerDB:
+.long		0
+_SysBase:
+.long		0
+_DOSBase:
+.long		0
+_PowerPCBase:
+.long		0
+			
